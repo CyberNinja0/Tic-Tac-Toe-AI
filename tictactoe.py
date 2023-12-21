697e3ba7ce15a74c2a25d65a2e4e67db96eb046b
Aniket Bhosale <108017771+ANI2707@users.noreply.github.com>
2023-12-21 20:36:09 +0530

    some functions created

--- tictactoe.py
+++ tictactoe.py
@@ -13,18 +13,75 @@ screen.fill(BG_COLOR)
 class Board:
     def __init__(self):
         self.squares = np.zeros((ROWS, COLS))
+        self.empty = self.squares
+        self.marked_sqrs = 0
+
+    def final_state(self):
+        """
+        @return 0 if there is no win yet
+        @return 1 if player 1 wins
+        @returb 2 if player 2 wins
+
+        """
+        # vertial wins
+        for col in range(COLS):
+            if (
+                self.squares[0][col]
+                == self.squares[1][col]
+                == self.squares[2][col]
+                != 0
+            ):
+                return self.squares[0][col]
+        # horizontal wins
+        for row in range(ROWS):
+            if (
+                self.squares[row][0]
+                == self.squares[row][1]
+                == self.squares[row][2]
+                != 0
+            ):
+                return self.squares[row][0]
+
+        #  desc diagonal
+        if self.squares[0][0] == self.squares[1][1] == self.squares[2][2] != 0:
+            return self.squares[1][1]
+        #  asc diagonal
+        if self.squares[2][0] == self.squares[1][1] == self.squares[0][2] != 0:
+            return self.squares[1][1]
+
+        #  no win yet
+        return 0
 
     def mark_sqr(self, row, col, player):
         self.squares[row][col] = player
+        self.marked_sqrs += 1
 
     def empty_sqr(self, row, col):
         return self.squares[row][col] == 0
+
+    def get_empty_sqrs(self):
+        empty_sqrs = []
+        for row in range(ROWS):
+            for col in range(COLS):
+                if self.empty_sqr(row, col):
+                    empty_sqrs.append((row, col))
+
+        return empty_sqrs
+
+    def isfull(self):
+        return self.marked_sqrs == 9
+
+    def isempty(self):
+        return self.marked_sqrs == 0
 
 
 class Game:
     def __init__(self):
         self.board = Board()
-        self.player = 1
+        # self.ai=AI()
+        self.player = 1 #1-cross #2-circles
+        self.gamemode='pvp' #pvp or ai
+        self.runnnig=True
         self.show_lines()
 
     def show_lines(self):
@@ -51,18 +108,18 @@ class Game:
         if self.player == 1:
             # draw cross
             #  desc line
-            start_desc=(col*SQSIZE + OFFSET , row*SQSIZE+OFFSET)
-            end_desc=(col*SQSIZE + SQSIZE-OFFSET , row*SQSIZE+SQSIZE-OFFSET)
-            pygame.draw.line(screen,CROSS_COLOR,start_desc,end_desc,CROSS_WIDTH)
+            start_desc = (col * SQSIZE + OFFSET, row * SQSIZE + OFFSET)
+            end_desc = (col * SQSIZE + SQSIZE - OFFSET, row * SQSIZE + SQSIZE - OFFSET)
+            pygame.draw.line(screen, CROSS_COLOR, start_desc, end_desc, CROSS_WIDTH)
             #  asc line
-            start_asc=(col*SQSIZE + OFFSET , row*SQSIZE+SQSIZE-OFFSET)
-            end_asc=(col*SQSIZE + SQSIZE-OFFSET , row*SQSIZE+OFFSET)
-            pygame.draw.line(screen,CROSS_COLOR,start_asc,end_asc,CROSS_WIDTH)
+            start_asc = (col * SQSIZE + OFFSET, row * SQSIZE + SQSIZE - OFFSET)
+            end_asc = (col * SQSIZE + SQSIZE - OFFSET, row * SQSIZE + OFFSET)
+            pygame.draw.line(screen, CROSS_COLOR, start_asc, end_asc, CROSS_WIDTH)
 
         elif self.player == 2:
             # draw circle
-            center=(col*SQSIZE+SQSIZE //2,row*SQSIZE+SQSIZE//2)
-            pygame.draw.circle(screen,CIRC_COLOR,center,RADIUS,CIRC_WIDTH)
+            center = (col * SQSIZE + SQSIZE // 2, row * SQSIZE + SQSIZE // 2)
+            pygame.draw.circle(screen, CIRC_COLOR, center, RADIUS, CIRC_WIDTH)
             pass
 
     def next_turn(self):
